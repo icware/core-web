c7478806c28115ec2fb25c22cb756dbaaef5849b
Ideilson Souza <ideilson@icware.com.br>
2024-05-15 19:44:58 -0300

    Update AuthLogin.vue
    Update AuthLogin

--- src/packages/Auth/views/AuthLogin.vue
+++ src/packages/Auth/views/AuthLogin.vue
@@ -1,11 +1,36 @@
+<script setup>
+import useAuthModel from '@/models/Auth';
+import { reactive } from 'vue';
+import { useRoute } from 'vue-router';
+import { AuthLogin } from '@/controllers/AuthController';
+
+const router = useRoute();
+const auth = useAuthModel();
+
+const requestData = reactive({
+    email: '',
+    password: ''
+});
+
+async function submit() {
+    try {
+        const response = await AuthLogin()
+    } catch (error) {
+
+    }
+}
+
+
+</script>
+
+
 <template lang="pt-br">
     <div>
        
     </div>
 </template>
-<script setup>
 
-</script>
+
 <style lang="">
 
 </style>
