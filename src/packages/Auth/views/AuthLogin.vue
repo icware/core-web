d0a3c6696b2453147530677039d1f7b87826aec3
Ideilson Souza <ideilson@icware.com.br>
2024-05-15 20:23:52 -0300

    Update AuthLogin.vue
    construção tela de login

--- src/packages/Auth/views/AuthLogin.vue
+++ src/packages/Auth/views/AuthLogin.vue
@@ -1,32 +1,34 @@
-<script setup>
-import useAuthModel from '@/models/Auth';
-import { reactive } from 'vue';
-import { useRoute } from 'vue-router';
-import { AuthLogin } from '@/controllers/AuthController';
-
-const router = useRoute();
-const auth = useAuthModel();
-
-const requestData = reactive({
-    email: '',
-    password: ''
-});
-
-async function submit() {
-    try {
-        const response = await AuthLogin()
-    } catch (error) {
-
-    }
-}
-
-
-</script>
-
-
-<template lang="pt-br">
+<template>
     <div>
-       
+        <form @submit.prevent="submitForm">
+
+            <div v-if="MessageError" role="alert">
+                {{ MessageError }}
+            </div>
+
+            <div>
+
+
+                <div>
+                    <label for=password>Email</label>
+                    <input type="email" v-model="data.email" name="email" id="email" autocomplete="email" />
+                </div>
+
+                <div>
+                    <label for=password>Senha</label>
+                    <input type="password" v-model="data.password" name="password" id="password"
+                        autocomplete="password" />
+                </div>
+
+                <div>
+                    <button type="submit">
+                        Entrar
+                    </button>
+                </div>
+
+            </div>
+
+        </form>
     </div>
 </template>
 
@@ -34,3 +36,48 @@ async function submit() {
 <style lang="">
 
 </style>
+
+<script setup>
+import useAuthModel from '@/models/Auth';
+import { onMounted, reactive, ref } from 'vue';
+import { useRoute } from 'vue-router';
+import { AuthLogin } from '@/controllers/AuthController';
+
+const router = useRoute();
+const auth = useAuthModel();
+const MessageError = ref('');
+
+
+const data = reactive({
+    email: '',
+    password: ''
+});
+
+async function submitForm() {
+    try {
+        const response = await AuthLogin(data.email, data.password);
+
+        if (response.status === 200) {
+            auth.setData(response.data);
+            router.push('/dashboard');
+        } else {
+            if (response.data) {
+                MessageError.value = response.data.error;
+            } else {
+                MessageError.value = 'Falha ao tentar fazer login';
+            }
+        }
+
+    } catch (error) {
+        MessageError.value = error;
+    }
+}
+
+onMounted(() => {
+    if (auth.getIsAuth) {
+        router.push('/dashboard');
+    }
+})
+
+
+</script>
